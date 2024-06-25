1fdb102c68ed5f4cd5c82c50efa4b2a9e3efcee8
Kelly <kellyvallecillo999@.com>
2024-06-25 14:25:38 -0600

    :zap: update forest model

--- src/modelos3d/model.py
+++ src/modelos3d/model.py
@@ -20,20 +20,19 @@ class BaseModel:
 
     def get_model_matrix(self):
         m_model = glm.mat4()
-        # translate
+        # translacion
         m_model = glm.translate(m_model, self.pos)
-        # rotate
+        # rotacion
         m_model = glm.rotate(m_model, self.rot.z, glm.vec3(0, 0, 1))
         m_model = glm.rotate(m_model, self.rot.y, glm.vec3(0, 1, 0))
         m_model = glm.rotate(m_model, self.rot.x, glm.vec3(1, 0, 0))
-        # scale
+        # scalacion
         m_model = glm.scale(m_model, self.scale)
         return m_model
 
     def render(self):
         self.update()
         self.vao.render()
-
 
 class ExtendedBaseModel(BaseModel):
     def __init__(self, app, vao_name, tex_id, pos, rot, scale):
@@ -55,19 +54,19 @@ class ExtendedBaseModel(BaseModel):
 
     def on_init(self):
         self.program['m_view_light'].write(self.app.light.m_view_light)
-        # resolution
+        # resolucion
         self.program['u_resolution'].write(glm.vec2(self.app.WIN_SIZE))
-        # depth texture
+        # textura
         self.depth_texture = self.app.mesh.texture.textures['depth_texture']
         self.program['shadowMap'] = 1
         self.depth_texture.use(location=1)
-        # shadow
+        # sombras
         self.shadow_vao = self.app.mesh.vao.vaos['shadow_' + self.vao_name]
         self.shadow_program = self.shadow_vao.program
         self.shadow_program['m_proj'].write(self.camera.m_proj)
         self.shadow_program['m_view_light'].write(self.app.light.m_view_light)
         self.shadow_program['m_model'].write(self.m_model)
-        # texture
+        # textura pt2
         self.texture = self.app.mesh.texture.textures[self.tex_id]
         self.program['u_texture_0'] = 0
         self.texture.use(location=0)
@@ -75,17 +74,16 @@ class ExtendedBaseModel(BaseModel):
         self.program['m_proj'].write(self.camera.m_proj)
         self.program['m_view'].write(self.camera.m_view)
         self.program['m_model'].write(self.m_model)
-        # light
+        # luz
         self.program['light.position'].write(self.app.light.position)
         self.program['light.Ia'].write(self.app.light.Ia)
         self.program['light.Id'].write(self.app.light.Id)
         self.program['light.Is'].write(self.app.light.Is)
 
-
+   #monteee
 class Cube(ExtendedBaseModel):
-    def __init__(self, app, vao_name='cube', tex_id=0, pos=(0, 0, 0), rot=(0, 0, 0), scale=(0.1, 0.1, 0.1)):
+    def __init__(self, app, vao_name='cube', tex_id=0, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
         super().__init__(app, vao_name, tex_id, pos, rot, scale)
-
 
 class MovingCube(Cube):
     def __init__(self, *args, **kwargs):
@@ -95,30 +93,33 @@ class MovingCube(Cube):
         self.m_model = self.get_model_matrix()
         super().update()
 
-
-# aca cambiamos la escala del museo
-# siguiendo esta misma logica pondremos mas modelos
-class Cat(ExtendedBaseModel):
+ # aca cambiamos la escala del museo
+class Cat(ExtendedBaseModel): #este es el museo
     def __init__(self, app, vao_name='cat', tex_id='cat',
                  pos=(0, 0, 0), rot=(0, 0, 0), scale=(0.3, 0.3, 0.3)):
         super().__init__(app, vao_name, tex_id, pos, rot, scale)
 
 
-##class Cesped(ExtendedBaseModel):
-##    def __init__(self, app, vao_name='cesped', tex_id='cesped',
-##                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
-##        super().__init__(app, vao_name, tex_id, pos, rot, scale)
-##
-##
-##
-##class detalles(ExtendedBaseModel):
-##    def __init__(self, app, vao_name='Detalles', tex_id='Detalles',
-##                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
-##        super().__init__(app, vao_name, tex_id, pos, rot, scale)
-##
-##
-##
-##
+
+#ahora para la fuente
+class Fuente(ExtendedBaseModel):      #modelo de la estatua
+    def __init__(self, app, vao_name='fuente', tex_id='fuente',
+                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(0.5, 0.5, 0.5)): #cambiar la escala del obj
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
+
+#mesa
+class Mesa(ExtendedBaseModel):      #modelo de la estatua
+    def __init__(self, app, vao_name='mesa', tex_id='mesa',
+                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)): #cambiar la escala del obj
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
+
+
+ #ahora para los raboles
+class Tree(ExtendedBaseModel):  # modelo de la estatua
+    def __init__(self, app, vao_name='tree', tex_id='tree',
+                 pos=(0, 0, 0), rot=(0, 0, 0), scale=(0.4, 0.4, 0.4)):  # cambiar la escala del obj
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
+
 
 class SkyBox(BaseModel):
     def __init__(self, app, vao_name='skybox', tex_id='skybox',
@@ -137,7 +138,6 @@ class SkyBox(BaseModel):
         # mvp
         self.program['m_proj'].write(self.camera.m_proj)
         self.program['m_view'].write(glm.mat4(glm.mat3(self.camera.m_view)))
-
 
 class AdvancedSkyBox(BaseModel):
     def __init__(self, app, vao_name='advanced_skybox', tex_id='skybox',
