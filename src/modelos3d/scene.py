7913833830f630eec749e982b584cf778d21f304
JaenR18 <170368614+JaenR18@users.noreply.github.com.>
2024-06-30 11:44:07 -0600

    getting the model positions

--- src/modelos3d/scene.py
+++ src/modelos3d/scene.py
@@ -23,16 +23,33 @@ class Scene:
             for z in range(-n, n, s):
                 add(Cube(app, pos=(x, -s, z)))
 
-#aca se edita la pos donde estara el obj
+        # Objetos
         add(Cat(app, pos=(0, -0.9, -5)))
-        add(Head(app, pos=(0, -0.3, -5)))
         add(Mesa(app, pos=(-0.3, -0.9, -4.3)))
-        add(Herku(app, pos=(-2.5, -0.25, -7.6)))
-        add(Woman(app, pos=(2.5, -0.90, -7.6)))
-        add(Man(app, pos=(0, -0.90, -7.6)))
         add(Jarra(app, pos=(-0.5, -0.53, -4.4)))
         add(Plato(app, pos=(0.5, -0.52, -4.4)))
+
+        # Estatuas
+        add(Head(app, pos=(0, -0.3, -5)))
+        self.app.positions_monument.append((0, -5))         # Ananke 0
+
+        add(Herku(app, pos=(-2.5, -0.25, -7.6)))
+        self.app.positions_monument.append((-2.5, -7.6))    # Hercules 1
+
+        add(Woman(app, pos=(2.5, -0.90, -7.6)))
+        self.app.positions_monument.append((2, -7.6))       # Afrodita 2
+
+        add(Man(app, pos=(0, -0.90, -7.6)))
+        self.app.positions_monument.append((0, -7.6))       # Man 3
+
         add(Orar(app, pos=(-2.5, -0.80, -4.5)))
+        self.app.positions_monument.append((-2.5, -4.5))    # statue 4
+
         add(Mujer(app, pos=(2.5, -0.90, -4.5)))
+        self.app.positions_monument.append((2.5, -4.5))    # Mujer 5
+
         add(Ate(app, pos=(2.5, -0.2, -2.5)))
+        self.app.positions_monument.append((2.5, -2.5))    # Atenea 6
+
         add(Zeus(app, pos=(-2.5, -0.90, -2.5)))
+        self.app.positions_monument.append((-2.5, -2.5))    # Zeus 7
